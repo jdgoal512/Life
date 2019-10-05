7b89f2e52b9ebbd5d21a3bb05a95c2e8d1865476
jdgoal512 <jonathandgleason@gmail.com>
2019-10-05 07:07:20 -0600

    Finished perl implementation

--- life.pl
+++ life.pl
@@ -4,37 +4,28 @@
 $WIDTH = 20;
 $HEIGHT = 10;
 
-use constant {
-    D => 0,
-    S => 1,
-    G => 2,
-};
- 
 # Rule per number of neighbors
-# D: die, G: grow, S: stay the same
+use constant {
+    D => 0,  # Die
+    S => 1,  # Stay the same
+    G => 2,  # Grow
+};
 #      0 1 2 3 4 5 6 7 8
 @RULES = (D, D, S, G, D, D, D, D, D);
 
-my @GRID;
-
-# declare -a GRID
-
-sub get {
-    my ($x, $y) = @_;
-    if ($x > 0 && $x <= $WIDTH && $y > 0 && $y <= $HEIGHT) {
-        return $GRID[$x*$WIDTH + $y];
-    }
-}
+my @Grid;
 
 sub get_index {
     my ($x, $y) = @_;
-    return $x*$WIDTH + $y
+    return $x*$WIDTH + $y;
 
 }
 
-sub set {
-    my ($x, $y, $value) = @_;
-    $GRID[get_index $x, $y] = $value;
+sub get {
+    my ($x, $y) = @_;
+    if ($x >= 0 && $x < $WIDTH && $y >= 0 && $y < $HEIGHT) {
+        return $Grid[get_index($x, $y)];
+    }
 }
 
 sub get_neighbors {
@@ -44,7 +35,7 @@ sub get_neighbors {
         for my $y_offset (-1 .. 1) {
             # Don't count the offset 0, 0
             if ($x_offset != $y_offset or $x_offset != 0) {
-                if (get $x+$x_offset, $y+$y_offset) {
+                if (get($x+$x_offset, $y+$y_offset)) {
                     $neighbors += 1
                 }
             }
@@ -54,27 +45,19 @@ sub get_neighbors {
 }
 
 sub print_grid {
-    printf "+";
-    for my $x (0 .. $WIDTH-1) {
-        printf "-";
-    }
-    printf "+\n";
+    print("+" . "-" x $WIDTH . "+\n");
     for my $y (0 .. $HEIGHT-1) {
-        printf "|";
+        print("|");
         for my $x (0 .. $WIDTH-1) {
-            if (get $x, $y) {
-                printf "0";
+            if (get($x, $y)) {
+                print("0");
             } else {
-                printf " ";
+                print(" ");
             }
         }
-        printf "|\n";
+        print("|\n");
     }
-    printf "+";
-    for my $x (0 .. $WIDTH-1) {
-        printf "-";
-    }
-    printf "+\n";
+    print("+" . "-" x $WIDTH . "+\n");
 }
 
 sub next_generation {
@@ -84,50 +67,42 @@ sub next_generation {
             my $neighbors = get_neighbors($x, $y);
             my $next_state = $RULES[$neighbors];
             if ($next_state == G) {
-                $next_grid[get_index $x, $y] = 1;
+                $next_grid[get_index($x, $y)] = 1;
             } elsif ($next_state == S) {
-                $next_grid[get_index $x, $y] = $grid[get_index $x, $y];
+                $next_grid[get_index($x, $y)] = $Grid[get_index($x, $y)];
             }
         }
     }
     for my $x (0 .. $WIDTH-1) {
         for my $y (0 .. $HEIGHT-1) {
-            set $x, $y, $next_grid[get_index $x, $y];
+            $Grid[get_index($x, $y)] = $next_grid[get_index($x, $y)];
         }
     }
 }
 
-# R-pentomino
-set 5, 5, 1;
-set 6, 5, 1;
-set 4, 6, 1;
-set 5, 6, 1;
-set 5, 7, 1;
+sub add_figure {
+    my $x = shift;
+    my $y = shift;
+    while (@_) {
+        my $a = shift;
+        my $b = shift;
+        $Grid[get_index($x+$a, $y+$b)] = 1;
+    }
+}
 
-# # Block
-# # set 5 5 1
-# # set 5 6 1
-# # set 6 5 1
-# # set 6 6 1
+@RPENTOMINO = ((1, 0), (2, 0), (0, 1), (1, 1), (1, 2));
+@BLOCK = ((0, 0), (1, 0), (0, 1), (1, 1));
+@BLINKER = ((1, 0), (1, 1), (1, 2));
+@BEACON = ((0, 0), (0, 1), (1, 0), (2, 3), (3, 2), (3, 3));
 
-# # Blinker
-# # set 5 4 1
-# # set 5 5 1
-# # set 5 6 1
-
-# # Beacon
-# # set 4 4 1
-# # set 4 5 1
-# # set 5 4 1
-# # set 6 7 1
-# # set 7 6 1
-# # set 7 7 1
+# add_figure(0, 0, @BLINKER);
+add_figure(4, 4, @RPENTOMINO);
 
 my $generation = 0;
 while (1) {
-    printf "Generation $generation\n";
-    print_grid;
-    next_generation;
+    print("Generation $generation\n");
+    print_grid();
+    next_generation();
     $generation += 1;
-    sleep 1
+    sleep(1)
 }
